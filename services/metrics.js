58a7ebb4fa9c844d21cd6047e0947b9b1ea27e7b
Michał Jezierski <ambinanitelo@gmail.com>
2015-04-20 11:18:51 +0200

    getting metrics from one-day time range

--- services/metrics.js
+++ services/metrics.js
@@ -2,8 +2,22 @@
 
 var repository = require('../repositories/elastic');
 
+function getTimeRange() {
+  var right, left = new Date();
+  left.setHours(0);
+  left.setMinutes(0);
+  left.setSeconds(0);
+  left.setMilliseconds(0);
+  right = left.getTime() + (24 * 3600000 - 1);
+
+  return {
+    left: left.getTime(),
+    right: right
+  }
+}
+
 module.exports = {
   get: function (index) {
-    return repository.get(index);
+    return repository.get(index, getTimeRange());
   }
 };
--- test/services/metrics.js
+++ test/services/metrics.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var sinon = require('sinon');
 var rewire = require('rewire');
 var service = rewire('../../services/metrics');
+var tk = require('timekeeper');
 
 describe("Metrics service", function () {
   beforeEach(function () {
@@ -12,9 +13,19 @@ describe("Metrics service", function () {
     this.service.__set__("repository", this.repository);
   });
 
-  it("get should retrieve metrics from metrics repository", function () {
+  it("should get metrics from expected repository index", function () {
     var spy = sinon.spy(this.repository, 'get');
     this.service.get("index");
     assert(spy.calledWith("index"));
+  });
+
+  it("should get metrics from expected time range", function () {
+    var spy = sinon.spy(this.repository, 'get');
+    tk.freeze(new Date(1444888000000)); // Thu, 15 Oct 2015 07:46:40 GMT+2
+    this.service.get("index");
+    assert(spy.calledWith("index", {
+      left: 1444860000000, // Thu, 15 Oct 2015 00:00:00 GMT+2
+      right: 1444946399999 // Thu, 15 Oct 2015 23:59:59 GMT+2
+    }));
   });
 });
