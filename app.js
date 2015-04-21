67cfdb191aa83c23ea8787e7e2f510f0bf168ac5
Michał Jezierski <ambinanitelo@gmail.com>
2015-04-21 12:13:13 +0200

    cookie parser removed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./lib/controllers/index');
@@ -15,7 +14,6 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
