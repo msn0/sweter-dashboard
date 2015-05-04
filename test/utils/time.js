1cbf3f832f0fe9d6db4adaa561b18ddeac559705
Michał Jezierski <ambinanitelo@gmail.com>
2015-05-05 00:26:10 +0200

    time api

--- test/utils/time.js
+++ test/utils/time.js
@@ -15,4 +15,13 @@ describe("Time range", function () {
   it("today should return expected range for particular day", function () {
     assert.deepEqual(time.today(), {left: 0, right: 86399999});
   });
+
+  it("last should return expected range for last n days", function () {
+    assert.deepEqual(time.last(7), {left: -518400000, right: 86399999});
+  });
+
+  it("last one day is the same as today", function () {
+    assert.deepEqual(time.last(1), time.today());
+  });
+
 });
